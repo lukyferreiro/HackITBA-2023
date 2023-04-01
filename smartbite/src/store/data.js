a41dcac6e73ed5d3951176e37494ece73f9e7508
Lucas Ferreiro <lferreiro@itba.edu.ar>
2023-04-01 11:13:37 -0400

    First creation of recipe model

--- smartbite/src/store/data.js
+++ smartbite/src/store/data.js
@@ -1,10 +1,11 @@
 const data = [
-    { id: 1, question: 'Longitud del plan', options:  },
+    { id: 1, question: 'Longitud del plan', options: ,  },
     { id: 2, question: 'Cantidad de comensales', price: 14.99 },
-    { id: 3, question: 'Tiene alergias o alguna problematica de salud ', price: 9.99 },
-    { id: 4, question: 'Cuanto tiempo tiene disponible ', price: 9.99 },
-    { id: 5, question: 'Cual es su objetivo principal ', price: 9.99 },
-    { id: 6, question: 'Alguna  ', price: 9.99 },
+    { id: 3, question: '', options: ,  },
+    { id: 4, question: 'Tiene alergias o alguna problematica de salud ', price: 9.99 },
+    { id: 5, question: 'Cuanto tiempo tiene disponible ', price: 9.99 },
+    { id: 6, question: 'Cual es su objetivo principal ', price: 9.99 },
+    { id: 7, question: 'Alguna  ', price: 9.99 },
   ];
   
   export default data;
