79f0d5335140ccc1e109a280031647c1218271c2
Mateo Perez Rivera <matperez@itba.edu.ar>
2023-04-02 05:56:00 -0300

    changes

--- smartbite/src/store/data.js
+++ smartbite/src/store/data.js
@@ -1,13 +1,10 @@
 const data = [
-    { id: '0', question: 'Cantidad de comensales', options: null , free: true },
-    { id: '1', question: 'Es usted vegano,vegetariano o keto',options: ['Vegano', 'Vegetariano'] , free: false},
+    { id: '1', question: 'Es usted vegano,vegetariano o keto',options: ['No','Vegano', 'Vegetariano','Keto'] , free: false},
     { id: '2', question: 'Sos alergico al mani', options: ['Si','No'] , free: false },
     { id: '3', question: 'Sos alergico a los mariscos', options: ['Si','No'] , free: false },
     { id: '4', question: 'Sos alergico a los lacteos', options: ['Si','No'] , free: false },
     { id: '5', question: 'Sufris de hipertencion:', options: ['Si','No'] , free: false },
     { id: '6', question: 'Sufris de diabetis:', options: ['Si','No'] , free: false },
-    { id: '7', question: 'Tiene 40 minutos libres para cocinar', options: ['Si', 'No'] , free: false },
-    { id: '8', question: 'Alguna condicion especial  ', options: null , free: true },
   ];
 
 const days_equiv = [
