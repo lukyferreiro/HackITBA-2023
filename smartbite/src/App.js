88cb460a033e422013d801765063a13128c8e426
Lucas Ferreiro <lferreiro@itba.edu.ar>
2023-04-02 02:22:35 -0400

    Forgot to delete an import

--- smartbite/src/App.js
+++ smartbite/src/App.js
@@ -1,7 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import LandingPage from "./pages/LandingPage";
-import PlanForm from "./pages/PlanForm";
 import PlanCompleted from "./pages/PlanCompleted";
 import Home from "./pages/Home";
 import RecetasRapidas from "./pages/RecetasRapidas";
